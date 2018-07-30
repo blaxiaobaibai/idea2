1b64255642b43a7338a57a0cb9a3516e83451187
shaofeng.liu <754475176@qq.com>
2018-07-30 16:11:34 +0800

    提交开始

--- service-hi/src/main/java/com/example/project/controller/LoginController.java
+++ service-hi/src/main/java/com/example/project/controller/LoginController.java
@@ -1,4 +1,31 @@
 package com.example.project.controller;
 
+import sun.misc.BASE64Encoder;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Calendar;
+import java.util.Date;
+
 public class LoginController {
+   public static void main(String [] args) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+
+       Calendar cal = Calendar.getInstance();
+       cal.setTime(new Date());
+       cal.add(Calendar.DATE, +2);
+       int week_index = cal.get(Calendar.DAY_OF_WEEK) - 1;
+       System.out.println(week_index);
+
+       String s=EncoderByMd5("123456qqq");
+       System.out.println(s);
+
+   }
+    public static String EncoderByMd5(String str) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
+        BASE64Encoder base64en = new BASE64Encoder();
+        byte[] md5Bytes = md5.digest(str.getBytes("utf-8"));
+        return base64en.encode(md5Bytes);
+    }
+
 }
